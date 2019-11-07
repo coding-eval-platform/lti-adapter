6eb388e50560f2ec13a70981254ccedf178e6ff5
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-11-06 23:37:02 -0300

    Remove all stuff created by archetype

--- lti-service-application/src/main/resources/db/migrations/V0_0_1_0__Initial_Schema.sql
+++ lti-service-application/src/main/resources/db/migrations/V0_0_1_0__Initial_Schema.sql
@@ -1,22 +1,3 @@
-CREATE TABLE users
-(
-    id       BIGSERIAL PRIMARY KEY NOT NULL,
-    username VARCHAR(64)           NOT NULL,
-    active   BOOLEAN               NOT NULL DEFAULT TRUE
-);
-
-CREATE UNIQUE INDEX users_username_unique_index
-    ON users (username);
-
-CREATE TABLE user_credentials
-(
-    id              BIGSERIAL PRIMARY KEY NOT NULL,
-    user_id         BIGINT                NOT NULL,
-    hashed_password VARCHAR               NOT NULL,
-    created_at      TIMESTAMP             NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
 CREATE TABLE tool_deployments
 (
     id                           UUID PRIMARY KEY NOT NULL,
--- lti-service-core/lti-service-models/src/main/java/ar/edu/itba/cep/lti_service/models/User.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.itba.cep.lti_service.models;
-
-/**
- * Represents a user of this application.
- */
-public class User {
-}

890fbb803c1ea2ad107559266234f17ac8ed4a4d
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-11-09 17:52:59 -0300

    Avoid the JwtStateHelperConfig create key beans
    - Now they are initialized in the JwtStateHelperConfig's constructor

--- lti-service-core/lti-service-domain/src/main/java/ar/edu/itba/cep/lti_service/domain/config/JwtStateHelperConfig.java
+++ lti-service-core/lti-service-domain/src/main/java/ar/edu/itba/cep/lti_service/domain/config/JwtStateHelperConfig.java
@@ -4,11 +4,9 @@ import ar.edu.itba.cep.lti_service.domain.helpers.AbstractJwtStateHelper;
 import ar.edu.itba.cep.lti_service.domain.helpers.ExamSelectionStateHelper;
 import ar.edu.itba.cep.lti_service.domain.helpers.LtiStateHelper;
 import ar.edu.itba.cep.security.KeyHelper;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -24,74 +22,48 @@ import java.security.spec.X509EncodedKeySpec;
 /**
  * Configuration class used to create {@link AbstractJwtStateHelper} subclasses beans.
  */
-@AllArgsConstructor
 @Configuration
 @EnableConfigurationProperties(JwtStateHelperConfig.JwtStateHelperProperties.class)
 public class JwtStateHelperConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JwtStateHelperConfig.class);
 
+    private final PrivateKey privateKey;
+    private final PublicKey publicKey;
+
+    public JwtStateHelperConfig(final JwtStateHelperProperties properties) {
+        this.publicKey = KeyHelper.generateKey(
+                getKeyFactory(),
+                properties.getPublicKey(),
+                X509EncodedKeySpec::new,
+                KeyFactory::generatePublic
+        );
+        this.privateKey = KeyHelper.generateKey(
+                getKeyFactory(),
+                properties.getPrivateKey(),
+                PKCS8EncodedKeySpec::new,
+                KeyFactory::generatePrivate
+        );
+    }
+
     /**
      * Builds an {@link LtiStateHelper} bean.
      *
-     * @param publicKey  The {@link PublicKey} needed by the {@link ExamSelectionStateHelper}.
-     * @param privateKey The {@link PrivateKey} needed by the {@link ExamSelectionStateHelper}.
      * @return The created bean.
      */
     @Bean
-    public LtiStateHelper ltiStateHelper(
-            @Qualifier(value = "state-helper-public-key") final PublicKey publicKey,
-            @Qualifier(value = "state-helper-private-key") final PrivateKey privateKey) {
+    public LtiStateHelper ltiStateHelper() {
         return new LtiStateHelper(publicKey, privateKey);
     }
 
     /**
      * Builds an {@link ExamSelectionStateHelper} bean.
      *
-     * @param publicKey  The {@link PublicKey} needed by the {@link ExamSelectionStateHelper}.
-     * @param privateKey The {@link PrivateKey} needed by the {@link ExamSelectionStateHelper}.
      * @return The created bean.
      */
     @Bean
-    public ExamSelectionStateHelper examCreationStateHelper(
-            @Qualifier(value = "state-helper-public-key") final PublicKey publicKey,
-            @Qualifier(value = "state-helper-private-key") final PrivateKey privateKey) {
+    public ExamSelectionStateHelper examCreationStateHelper() {
         return new ExamSelectionStateHelper(publicKey, privateKey);
-    }
-
-
-    /**
-     * Builds a {@link PublicKey} from the given {@code encoded} {@link String} representation of the key.
-     *
-     * @param properties The {@link JwtStateHelperProperties} from where the encoded public key is taken.
-     * @return The created {@link PublicKey}.
-     */
-    @Bean
-    @Qualifier(value = "state-helper-public-key")
-    public PublicKey publicKey(final JwtStateHelperProperties properties) {
-        return KeyHelper.generateKey(
-                getKeyFactory(),
-                properties.getPublicKey(),
-                X509EncodedKeySpec::new,
-                KeyFactory::generatePublic
-        );
-    }
-
-    /**
-     * Builds a {@link PrivateKey} from the given {@code encoded} {@link String} representation of the key.
-     *
-     * @param properties The {@link JwtStateHelperProperties} from where the encoded private key is taken.
-     * @return The created {@link PrivateKey}.
-     */
-    @Bean
-    @Qualifier(value = "state-helper-private-key")
-    public PrivateKey privateKey(final JwtStateHelperProperties properties) {
-        return KeyHelper.generateKey(
-                getKeyFactory(),
-                properties.getPrivateKey(),
-                PKCS8EncodedKeySpec::new,
-                KeyFactory::generatePrivate
-        );
     }
 
 
